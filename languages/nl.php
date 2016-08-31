8a16f43ebe1a71580b436abf81b5e78626ba3c6b
Jerome Bakker <jeabakker@coldtrick.com>
2016-08-31 17:11:29 +0200

    chore: Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * This file was created by Translation Editor v5.1
+ * On 2016-08-31 17:10
+ */
+
+return array (
+  'account_removal' => 'Account verwijderen',
+  'account_removal:menu:title' => 'Account verwijderen',
+  'account_removal:disable:default' => 'Dit account is uitgeschakeld op het verzoek van de gebruiker',
+  'account_removal:admin:settings:user_options' => 'Selecteer de beschikbare opties voor account verwijderen',
+  'account_removal:admin:settings:user_options:disable' => 'Uitschakelen',
+  'account_removal:admin:settings:user_options:remove' => 'Verwijderen',
+  'account_removal:admin:settings:user_options:disable_and_remove' => 'Uitschakelen en Verwijderen',
+  'account_removal:admin:settings:groupadmins_allowed' => 'Groepseigenaren kunnen zichzelf uitschakelen / verwijderen',
+  'account_removal:admin:settings:reason_required' => 'Reden voor account verwijderen is verplicht voor gebruikers',
+  'account_removal:user:error:admin' => 'Beheerders mogen zichzelf niet uitschakelen / verwijderen',
+  'account_removal:user:error:user' => 'Je kunt alleen jezelf uitschakelen / verwijderen',
+  'account_removal:user:error:no_user' => 'Ongeldige invoer; Gebruiker niet gevonden',
+  'account_removal:user:title' => 'Account verwijderen',
+  'account_removal:forms:user:user_options:description:disable' => 'Hier kun je je account <b>uitschakelen</b>. Dit zal ervoor zorgen dat je profiel wordt uitgeschakeld. Dit betekend dat je profiel niet langer bekeken kan worden en dat je account niet meer voorkomt in gebruikers overzichten.
+
+Al je content (zoals blogs, bestanden en pagina\'s) blijven beschikbaar voor andere gebruikers.',
+  'account_removal:forms:user:user_options:description:remove' => 'Hier kun je je account <b>verwijderen</b>. Dit zal ervoor zorgen dat je profiel volledig wordt verwijderd. Dit betekend dat je profiel niet langer bekeken kan worden en dat je account niet meer voorkomt in gebruikers overzichten.
+
+Tevens zal <b>al</b> je content (zoals blogs, bestanden en pagina\'s) worden verwijderd. Deze actie kan <b>niet</b> ongedaan worden gedaan!',
+  'account_removal:forms:user:user_options:description:disable_and_remove' => 'Hier kun je je account <b>uitschakelen</b> of <b>verwijderen</b>. Dit zal ervoor zorgen dat je profiel wordt uitgeschakeld of verwijderd. Dit betekend dat je profiel niet langer bekeken kan worden en dat je account niet meer voorkomt in gebruikers overzichten.
+
+Als je ervoor kiest om je account <b>uit te schakelen</b> zal al je content (zoals blogs, bestanden en pagina\'s) beschikbaar blijven voor andere gebruikers.
+
+Als je ervoor kiest om je account te <b>verwijderen</b> zal all je content  (zoals blogs, bestanden en pagina\'s) worden verwijderd. Deze actie kan <b>niet</b> ongedaan worden gedaan!',
+  'account_removal:forms:user:user_options:description:disable_and_remove:choice' => 'Ik wil',
+  'account_removal:forms:user:user_options:description:general' => 'Als je dit verzoek om je account te verwijderen verstuurd, zal er een bevestiging e-mail worden verstuurd naar je e-mail inbox. Het verwijderen van je account zal pas worden afgerond als je de instructies in de e-mail volgt.',
+  'account_removal:forms:user:user_options:disable' => 'Mijn account uitschakelen',
+  'account_removal:forms:user:user_options:remove' => 'Mijn account verwijderen',
+  'account_removal:forms:user:reason' => 'Geef een reden op waarom je je account wilt verwijderen',
+  'account_removal:forms:user:js:confirm:disable' => 'Weet je zeker dat je dit account wilt uitschakelen?',
+  'account_removal:forms:user:js:confirm:remove' => 'Weet je zeker dat je dit account wilt verwijderen?',
+  'account_removal:forms:user:error:group_owner' => 'Je kunt op dit moment je account niet uitschakelen of verwijderen, omdat je nog de eigenaar bent van de volgende groepen.',
+  'account_removal:message:disable:subject' => 'Je wilt je account op %s uitschakelen',
+  'account_removal:message:disable:body' => 'Beste %s,
+
+Je hebt aangegeven je account uit te willen schakelen.
+Alle content die je gemaakt hebt op de site zal beschikbaar blijven voor andere gebruikers. Je kunt je niet langer aanmelden op de site, je account komt niet meer voor in gebruikers overzichten en je profiel is niet langer toegankelijk.
+
+Om te bevestigen dat je daadwerkelijk je account wilt uitschakelen klik op de onderstaande link:
+%s
+
+We hopen dat je hebt genoten van onze community en bedanken je voor je bijdrage.',
+  'account_removal:message:remove:subject' => 'Je wilt je account op %s verwijderen',
+  'account_removal:message:remove:body' => 'Beste %s,
+
+Je hebt aangegeven je account te willen verwijderen.
+Alle content die je gemaakt hebt op de site zal hiermee ook worden verwijderd.
+
+Om te bevestigen dat je daadwerkelijk je account wilt verwijderen klik op de onderstaande link:
+%s
+
+We hopen dat je hebt genoten van onze community en bedanken je voor je bijdrage.',
+  'account_removal:message:thank_you:remove:subject' => 'Bedankt voor het gebruiken van %s',
+  'account_removal:message:thank_you:remove:body' => 'Beste %s,
+
+We willen je bedanken voor het gebruik van %s. We hopen dat je hebt genoten van onze community.
+
+Als je ooit nog weer eens wilt terug keren, kun je altijd een nieuw account aanmaken.
+
+Nogmaals hartelijk dank voor je bijdrage.',
+  'account_removal:message:thank_you:disable:subject' => 'Bedankt voor het gebruiken van %s',
+  'account_removal:message:thank_you:disable:body' => 'Beste %s,
+
+We willen je bedanken voor het gebruik van %s. We hopen dat je hebt genoten van onze community.
+
+Als je ooit nog weer eens wilt terug keren, kun je altijd een nieuw account aanmaken of vragen aan een van de site beheerder om je account te heractiveren.
+
+Nogmaals hartelijk dank voor je bijdrage.',
+  'account_removal:actions:remove:error:user_guid:admin' => 'Beheerders mogen zichzelf niet uitschakelen / verwijderen',
+  'account_removal:actions:remove:error:group_owner' => 'Je kunt jezelf niet uitschakelen of verwijderen aangezien je nog de eigenaar bent van de volgende groepen',
+  'account_removal:actions:remove:error:reason' => 'Geef een reden op',
+  'account_removal:actions:remove:error:type_match' => 'De opgegeven verwijder optie is niet toegestaan',
+  'account_removal:actions:remove:error:token_mismatch' => 'De opgegeven bevestiging tokens zijn niet geldig. Vraag een nieuwe bevestiging e-mail op.',
+  'account_removal:actions:remove:success:remove' => 'Je account is succesvol verwijderd',
+  'account_removal:actions:remove:success:disable' => 'Je account is succesvol uitgeschakeld',
+  'account_removal:actions:remove:success:request' => 'Je hebt aangevraagd om je account uit te schakelen of te verwijderen. Controleer je e-mail inbox voor een bevestiging link.',
+);
