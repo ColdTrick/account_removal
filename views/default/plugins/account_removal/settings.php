b013f99000ea61615bf28e08e5045e7046439950
Rohit Gupta <rohit_1290@yahoo.com>
2019-08-08 23:42:58 +0530

    changed: code cleanup and update

--- views/default/plugins/account_removal/settings.php
+++ views/default/plugins/account_removal/settings.php
@@ -2,47 +2,47 @@
 
 $plugin = $vars["entity"];
 
-$noyes_options = array(
+$noyes_options = [
 	"no" => elgg_echo("option:no"),
 	"yes" => elgg_echo("option:yes"),
-);
+];
 
-$user_options = array(
+$user_options = [
 	"disable" => elgg_echo("account_removal:admin:settings:user_options:disable"),
 	"remove" => elgg_echo("account_removal:admin:settings:user_options:remove"),
 	"disable_and_remove" => elgg_echo("account_removal:admin:settings:user_options:disable_and_remove"),
-);
+];
 
 ?>
 <div>
 	<?php
 		echo elgg_echo("account_removal:admin:settings:user_options");
-		echo "&nbsp;" . elgg_view("input/dropdown", array(
+		echo "&nbsp;" . elgg_view("input/dropdown", [
 			"name" => "params[user_options]",
 			"options_values" => $user_options,
 			"value" => $plugin->user_options
-		));
+		]);
 	?>
 </div>
 
 <div>
 	<?php
 		echo elgg_echo("account_removal:admin:settings:groupadmins_allowed");
-		echo "&nbsp;" . elgg_view("input/dropdown", array(
+		echo "&nbsp;" . elgg_view("input/dropdown", [
 			"name" => "params[groupadmins_allowed]",
 			"options_values" => $noyes_options,
 			"value" => $plugin->groupadmins_allowed
-		));
+		]);
 	?>
 </div>
 
 <div>
 	<?php
 		echo elgg_echo("account_removal:admin:settings:reason_required");
-		echo "&nbsp;" . elgg_view("input/dropdown", array(
+		echo "&nbsp;" . elgg_view("input/dropdown", [
 			"name" => "params[reason_required]",
 			"options_values" => $noyes_options,
 			"value" => $plugin->reason_required
-		));
+		]);
 	?>
 </div>
